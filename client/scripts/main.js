45ddc4e0db869011bd7919c109279a5ff5419b08
Amey <ameyparab2007@gmail.com>
2016-01-12 11:34:52 -0800

    Added AngularJS locale support (fr-fr & en)

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,5 +1,5 @@
 // get the locale from localStorgae. If no locale is set, then use English as default
-var locale = localStorage.getItem('locale') || 'en-en';
+var locale = localStorage.getItem('locale') || 'en';
 localStorage.setItem('locale', locale);
 
 require.config({
@@ -7,13 +7,15 @@ require.config({
 	
 	paths: {
 		angular: "static/lib/angular.min",
+		angularLocale: "static/lib/i18n/angular-locale_" + locale,
         ngRoute: "static/lib/angular-route.min",
 		app: 'static/scripts/app'
 	},
 	
 	shim: {
 		'angular' : {'exports' : 'angular'},
-		'angularRoute': ['angular']
+		'angularRoute': ['angular'],
+		'angularLocale': ['angular']
 	},
 	
 	priority: [
@@ -29,8 +31,9 @@ require.config({
 
 require([
 	'angular',
+	'angularLocale',
 	'app'
-	], function(angular, app) {
+	], function(angular, angularLocale, app) {
 		var $html = angular.element(document.getElementsByTagName('html')[0]);
 		angular.element().ready(function() {
 			// bootstrap the app manually
